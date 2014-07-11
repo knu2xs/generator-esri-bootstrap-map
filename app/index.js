1b034fdf902cd194429411d2dcb4c62d273c612d
knu2xs <knu2xs@gmail.com>
2014-07-11 14:34:48 -0400

    grunt copy functional

--- app/index.js
+++ app/index.js
@@ -21,7 +21,7 @@ var EsriBootstrapMapGenerator = yeoman.generators.Base.extend({
             {
                 type: 'list',
                 name: 'template',
-                message: 'Which EsriBootstrapMap template would you like to install?',
+                message: 'Which esri-bootstrap-map template would you like to install?',
                 choices: [
                     'fullmap',
                     'geosearch',
@@ -39,28 +39,29 @@ var EsriBootstrapMapGenerator = yeoman.generators.Base.extend({
 
     scafooldFolders: function () {
         this.mkdir('app');
-        this.mkdir('app/style');
-        this.mkdir('app/style/fonts');
-        this.mkdir('app/javascript');
+        this.mkdir('app/css');
+        this.mkdir('app/fonts');
+        this.mkdir('app/js');
     },
 
     copyProjectFiles: function () {
 
         // general yeoman/bower/node/git resources
-        this.copy('_package.json', 'package.json');
-        this.copy('_bower.json', 'bower.json');
+        this.copy('package.json', 'package.json');
+        this.copy('bower.json', 'bower.json');
         this.copy('.gitignore', '.gitignore');
+        this.copy('Gruntfile.js', 'Gruntfile.js');
 
         // bootstrap map resources
-        this.copy('_bootstrapmap.js', 'app/javascript/bootstrapmap.js');
-        this.copy('_dojo-config.js', 'app/javascript/dojo-config.js');
-        this.copy('_bootstrapmap.css', 'app/style/bootstrapmap.css');
+        this.copy('_bootstrapmap.js', 'app/js/bootstrapmap.js');
+        this.copy('_dojo-config.js', 'app/js/dojo-config.js');
+        this.copy('_bootstrapmap.css', 'app/css/bootstrapmap.css');
 
         // template specific resources
         var sourceDir = 'map-' + this.template;
         this.copy(sourceDir + '/_index.html', 'app/index.html');
-        this.copy(sourceDir + '/_style.css', 'app/style/map-style.css');
-        this.copy(sourceDir + '/_map.js', 'app/javascript/map.js');
+        this.copy(sourceDir + '/_style.css', 'app/css/map-style.css');
+        this.copy(sourceDir + '/_map.js', 'app/js/map.js');
     },
 
     getDependencies: function() {
@@ -72,27 +73,6 @@ var EsriBootstrapMapGenerator = yeoman.generators.Base.extend({
         this.npmInstall('', function(){
             console.log('\nSuccessfully installed NPM dependencies.');
         });
-    },
-
-    copyDownloadedResources: function() {
-        console.log('starting');
-        // set the source to the bower_components directory
-        generator.sourceRoot(this.dest + '../bower_components');
-        console.log(generator.src);
-
-        // copy bootstrap components
-        this.copy('bootstrap/dist/js/bootstrap.min.js', 'javascript/bootstrap.min.js');
-        var fonts = ['.eot', '.svg', '.ttf', '.woff'];
-        for (var i=0; i < fonts.length; i++){
-            this.copy(
-                'bootstrap/dist/fonts/glyphicons-halflings-regular' + fonts[i],
-                'style/fonts/glyphicons-halflings-regular' + fonts[i]
-            );
-        }
-        this.copy('bootstrap/dist/css/bootstrap.css', 'style/bootstrap.css');
-
-        // copy jquery
-        this.copy('jquery/dist/jquery.min.js', 'javascript/jquery.min.js');
     }
 });
 
--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -2,43 +2,65 @@
 
 module.exports = function (grunt) {
 
-    // timing, to show elapsed time at end
-    require('time-grunt')(grunt);
-
-    // load all grunt tasks
+    //instead of loadNpmTasks, load all dev dependencies from the package.json
     require('load-grunt-tasks')(grunt);
 
-    // configurable output paths
-    var yeomanConfig = {
-        app: 'app',
-        dist: 'dist'
-    };
-
-    // grunt configuration
     grunt.initConfig({
-        yeoman: yeomanConfig,
-        connect: {
-            server: {
-                options: {
-                    port: 9001,
-                    base: 'app'
+
+        // read package file for properties
+        pkg: grunt.file.readJSON('package.json'),
+
+        // copy files from downloaded packages into directories
+        copy: {
+            bootstrap: {
+                files: {
+                    'app/js/lib/bootstrap.min.js': 'bower_components/bootstrap/dist/js/bootstrap.min.js',
+                    'app/css/bootstrap.min.css': 'bower_components/bootstrap/dist/css/bootstrap.min.css',
+                    'app/fonts/glyphicons-halflings-regular.eot': 'bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot',
+                    'app/fonts/glyphicons-halflings-regular.svg': 'bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg',
+                    'app/fonts/glyphicons-halflings-regular.ttf': 'bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf',
+                    'app/fonts/glyphicons-halflings-regular.woff': 'bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff'
                 }
-            }
-        },
-        watch: {
-            appFiles: {
-                files: ['app/**/*'],
-                tasks: ['compass'],
-                options: {
-                    livereload: true
+            },
+            jquery: {
+                files: {
+                    'app/js/lib/jquery.min.js': 'bower_components/jquery/dist/jquery.min.js'
                 }
             }
         }
     });
 
+    // configurable output paths
+//    var yeomanConfig = {
+//        app: 'app',
+//        dist: 'dist'
+//    };
+
+    // grunt configuration
+//    grunt.initConfig({
+//        yeoman: yeomanConfig,
+//        connect: {
+//            server: {
+//                options: {
+//                    port: 9001,
+//                    base: 'app'
+//                }
+//            }
+//        },
+//        watch: {
+//            appFiles: {
+//                files: ['app/**/*'],
+//                tasks: ['compass'],
+//                options: {
+//                    livereload: true
+//                }
+//            }
+//        }
+//    });
+
     // tell grunt what plugins to use
-    grunt.loadNpmTasks('grunt-contrib-watch');
+//    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // register tasks
-    grunt.registerTask('serve', ['connect']);
-}
+//    grunt.registerTask('serve', ['connect']);
+};
